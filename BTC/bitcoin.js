35753d940d6471b812ee61a446364207967bcf9c
Taiiwo <tylerzacc@gmail.com>
2013-09-06 15:55:17 +0100

    Update bitcoin.js
    Cleaned up the code a lot. Added and update function to update all of the stats, and an every second function to do stuff each second. I removed the function defined within pickaxe().

--- BTC/bitcoin.js
+++ BTC/bitcoin.js
@@ -1,38 +1,34 @@
 var counter = 0;
 var pickaxes = 0;
+var codeallthetime = setInterval(everysecond(),1000);// Run code every second
 
 function addone() {
     counter = counter + 5;
-    $("h1").html(counter);
-
-    if (counter == 10) {
-   $("p").html("Just 10? lel fag");
+    update();
+    if (counter >= 10 && counter <= 24) {
+        $("p").html("Just 10? lel fag");
     }
-     if (counter == 25) {
-   $("p").html("You don't scare me");
+    if (counter >= 25 && counter <= 49) {
+        $("p").html("You don't scare me");
     }
-     if (counter == 50) {
-   $("p").html("Seriously you can't do shit with just 50 satoshi");
-    }
-    if (counter >= 50 ){
+    if (counter >= 50) {
+        $("p").html("Seriously you can't do shit with just 50 satoshi");
         document.getElementById("button").disabled = false;
+}
+function update(){// Update the stats on the page
+    $("h2").html(pickaxes);
+    $("h1").html(counter);
+}
+
+function pickaxe() {
+    if (counter >= 50) {// If they have enough
+        pickaxes = pickaxes +1;// Give them a pickaxe
+        counter = counter - 50;// Charge them for it
+        update();
     }
 }
-
-function upgrade() {
-    $("h1").html(counter - 50);
-    $("h1").html(counter + 100);
+function everysecond() {
+    counter = counter + (1 * pickaxes);
+    update();
 }
-
-  function pickaxe() {
-      pickaxes = pickaxes +1;
-       $("h2").html(pickaxes);
-       $("h1").html(counter - 50);
-       var pickaxeTimer = setInterval(function() {
-          $('h1').html(counter+= 1);
-          if (pickaxes == 0 ) {
-              clearInterval(pickaxeTimer);
-          }
-      },1000);
-   }
 
