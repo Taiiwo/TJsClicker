0cf6e4cf567544515b69eb99573912330bb97570
Taiiwo Llort <Tylerzacc@gmail.com>
2013-09-06 17:13:28 +0100

    More sup3r updates

--- BTC/bitcoin.js
+++ BTC/bitcoin.js
@@ -1,34 +1,37 @@
 var counter = 0;
 var pickaxes = 0;
-var codeallthetime = setInterval(everysecond(),1000);// Run code every second
+var pickaxeprice = 50;
+var codeallthetime = setInterval(function(){everysecond();}, 1000); // Run code every second
 
 function addone() {
-    counter = counter + 5;
+    counter = (counter + 5);
     update();
-    if (counter >= 10 && counter <= 24) {
-        $("p").html("Just 10? lel fag");
+    if (counter >= 10 && counter <= 24); {
+        $("#message").html("Just 10? lel fag");
     }
     if (counter >= 25 && counter <= 49) {
-        $("p").html("You don't scare me");
+        $("#message").html("You don't scare me");
     }
     if (counter >= 50) {
-        $("p").html("Seriously you can't do shit with just 50 satoshi");
-        document.getElementById("button").disabled = false;
+        $("#message").html("Seriously you can't do shit with just 50 satoshi");
+    }
 }
-function update(){// Update the stats on the page
+function update() { // Update the stats on the page
     $("h2").html(pickaxes);
     $("h1").html(counter);
+    $("#price").html('$' + pickaxeprice);
 }
 
 function pickaxe() {
-    if (counter >= 50) {// If they have enough
-        pickaxes = pickaxes +1;// Give them a pickaxe
-        counter = counter - 50;// Charge them for it
+    if (counter >= 50) { // If they have enough
+        pickaxes = pickaxes + 1; // Give them a pickaxe
+        counter = counter - pickaxeprice; // Charge them for it
+	pickaxeprice = pickaxeprice * 1.5// Increase the price
         update();
     }
 }
+
 function everysecond() {
     counter = counter + (1 * pickaxes);
     update();
 }
-
