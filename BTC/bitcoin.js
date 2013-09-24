1c9402f312b3992baf529d0433d00b80b7004199
unknown <40830647@hb9576.academic.ncn.ac.uk>
2013-09-24 10:31:38 +0100

    Add more features

--- BTC/bitcoin.js
+++ BTC/bitcoin.js
@@ -1,12 +1,14 @@
 var counter = 0;
 var pickaxes = 0;
-var pickaxeprice = 50;
+var pickaxeprice = 2;
 var drills = 0;
-var drillprice = 200;
-var codeallthetime = setInterval(function(){everysecond();}, 1000); // Run code every second
+var drillprice = 20;
+var cursors=1;
+var cursorprice=5;
+var codeallthetime = setInterval(function(){everysecond();}, 10); // Run code every second
 
 function addone() {
-    counter = (counter + 1);
+    counter = (counter + (cursors/10));
     update();
     if (counter >= 10 && counter <= 24); {
         $("#message").html("Just 10? lel fag");
@@ -21,9 +23,11 @@ function addone() {
 function update() { // Update the stats on the page
     $("#pickstat").html(pickaxes);
     $("#drillstat").html(drills);
+	$("#cursorstat").html(cursors);
     $("h1").html(Math.round(counter * 100)/100);
     $("#price").html('$' + pickaxeprice);
     $("#drill").html('$' + drillprice);
+	$("#cursor").html('$' + cursorprice);
 }
 
 function pickaxe() {
@@ -42,7 +46,15 @@ function drill() {
         update();
     }
 }
+function cursor() {
+    if (counter >= cursorprice) { // If they have enough
+        cursors++; // Give them a cursor
+        counter = counter - cursorprice; // Charge them for it
+        cursorprice = Math.round((cursorprice * 2)*100)/100;// Increase and round the price
+        update();
+    }
+}
 function everysecond() {
-    counter = counter + (1 * pickaxes) + (5 * drills);
+    counter = counter + (0.001 * pickaxes) + (0.005 * drills);
     update();
 }
