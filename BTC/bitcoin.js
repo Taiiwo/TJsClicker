fa7545fe51cfb2ea804c28211c9975d5d0dcecf3
Taiiwo Llort <tylerzacc@gmail.com>
2013-10-04 22:15:35 +0100

    Super commit. The whole page is restructured.

--- BTC/bitcoin.js
+++ BTC/bitcoin.js
@@ -1,18 +1,43 @@
 var counter = 0;
-var pickaxes = 0;
-var pickaxeprice = 5;
-var pickaxedurnum = 100;
-var pickaxedur = 0;
-var drills = 0;
-var drillprice = 20;
-var drilldurnum = 75;
-var drilldur = 0;
-var cursors=1;
-var cursorprice=10;
 var codeallthetime = setInterval(function(){everysecond();}, 10); // Run code every 10 milisecods
 
+var items = [
+		{
+			'name':'Pickaxe',
+			'price':5,
+			'curPrice':5,
+			'durabilityOrigin':200,
+			'quantity':0,
+			'durabilityCurrent':0,
+			'decay':0.01,
+			'power':0.0005
+		},
+		{
+			'name':'Drill',
+			'price':20,
+			'curPrice':20,
+			'durabilityOrigin':75,
+			'quantity':0,
+			'durabilityCurrent':0,
+			'decay':0.01,
+			'power':0.001
+		},
+
+		{
+			'name':'Cursor',
+			'price':10,
+			'curPrice':10,
+			'durabilityOrigin':500,
+			'quantity':1,
+			'durabilityCurrent':500,
+			'decay':0.01,
+			'power':0
+		}
+		
+];
+
 function addone() {
-    counter = (counter + (cursors/10));
+    counter = (counter + (items[2]['quantity']/10));
     update();
     if (counter >= 10 && counter <= 24); {
         $("#message").html("Just 10? lel fag");
@@ -25,54 +50,53 @@ function addone() {
     }
 }
 function update() { // Update the stats on the page
-    $("#pickstat").html(pickaxes);
-    $("#drillstat").html(drills);
-	$("#cursorstat").html(cursors);
-    $("h1").html(Math.round(counter * 100)/100);
-    $("#price").html('$' + pickaxeprice);
-    $("#drill").html('$' + drillprice);
-	$("#cursor").html('$' + cursorprice);
+	for (var i = 0; i < items.length; i++) {
+		$("#" + items[i]['name'] + "stat").html(items[i]['name'] + 's: ' + items[i]['quantity']);
+		$("#" + items[i]['name']).html(items[i]['name'] + ': $' + items[i]['curPrice']);
+	}
+	$("h1").html(counter.toFixed(2));
+}
+function makeButtons(){
+	var button;
+	var buttons = "";
+	for (var i = 0; i < items.length; i++) {
+		button = '<button onclick="buy(' + i + ')" ' + 'id="' + items[i]['name'] + '"></button>\n';
+		buttons += button;
+	}
+	$("#upgrades").html(buttons);
+}
+function makeStats(){
+        var stat;
+        var stats = '<p>BTC:</p><h1>' + counter + '</h1>';
+        for (var i = 0; i < items.length; i++) {
+                stat = '<p class="statname" id="' + items[i]['name'] +'stat">' + items[i]['name'] + 's: ' + items[i]['quantity'] + '</p><br />\n';
+                stats += stat;
+        }
+	console.log(stats);
+	$("#stats").html(stats);
+}
+function initialize() {//A function to run once on page load
+	makeStats();
+	makeButtons();
 }
 
-function pickaxe() {
-    if (counter >= pickaxeprice) { // If they have enough
-        pickaxes = pickaxes + 1; // Give them a pickaxe
-		pickaxedur = pickaxedur + pickaxedurnum;
-        counter = counter - pickaxeprice; // Charge them for it
-        pickaxeprice = Math.round((pickaxeprice * (1 + (pickaxes/10)))*100)/100;// Increase and round the price
-        update();
-    }
-}
-function drill() {
-    if (counter >= drillprice) { // If they have enough
-        drills = drills + 1; // Give them a drill
-		drilldur = drilldur + drilldurnum;
-        counter = counter - drillprice; // Charge them for it
-        drillprice = Math.round((drillprice * (1 + (drills/10)) )*100)/100;// Increase and round the price
-        update();
-    }
-}
-function cursor() {
-    if (counter >= cursorprice) { // If they have enough
-        cursors++; // Give them a cursor
-        counter = counter - cursorprice; // Charge them for it
-        cursorprice = Math.round((cursorprice * (1 + (cursors/10)))*100)/100;// Increase and round the price
-        update();
+function buy(id) {
+    if (counter >= items[id]['curPrice']) { // If they have enough
+        items[id]['quantity']++; // Give them a item
+	items[id]['durabilityCurrent'] = items[id]['durabilityCurrent'] + items[id]['durabilityOrigin'];// raise thir durability
+        counter = counter - items[id]['curPrice']; // Charge them for it
+        items[id]['curPrice'] = Math.pow(items[id]['price'], (1 + (items[id]['quantity']/10))).toFixed(2);// Increase and round the price
+	update();
     }
 }
 function everysecond() {
-    counter = counter + (0.0005 * pickaxes);
-	counter = counter + (0.001 * drills);
-	
-	pickaxedur = pickaxedur - (0.01 * pickaxes);
-	if (pickaxedur <= (pickaxedurnum * pickaxes) - pickaxedurnum && pickaxes > 0) {
-		pickaxes = pickaxes - 1;
+	for (var i = 0; i < items.length; i++) {
+		counter = counter + (items[i]['power'] * items[i]['quantity']);
+		items[i]['durabilityCurrent'] = items[i]['durabilityCurrent'] - (items[i]['decay'] * items[i]['quantity']);
+		if (items[i]['durabilityCurrent'] <= (items[i]['durabilityOrigin'] * (items[i]['quantity'] - 1)) && items[i]['quantity'] > 0) {
+			items[i]['quantity'] = items[i]['quantity'] - 1;
+			items[i]['curPrice'] = Math.pow(items[i]['curPrice'],1/1.1).toFixed(2);
+		}
 	}
-	drilldur = drilldur - (0.01 * drills);
-	if (drill <= (drilldurnum * drills) - drilldurnum && drills > 0) {
-		drill = drill - 1;
-	}
-	
-	
-    update();
+	update();
 }
